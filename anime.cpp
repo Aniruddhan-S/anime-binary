41ef77479f7cefae46596fe3aeeea01ad9d3f9de
Igneel001 <aniruddhan.suresh@gmail.com>
2021-04-22 00:18:21 +0530

    Added anime season

--- anime.cpp
+++ anime.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <conio.h>
 #include <fstream>
 #include <cstring>
 
@@ -8,6 +7,7 @@ using namespace std;
 struct Anime_list{
     char anime_name[100];
     char completed;
+    int season;
     int recent_ep_watched;
     char manga;
     int recent_chap_read;
@@ -15,18 +15,21 @@ struct Anime_list{
 
 int main(){
     
-    ifstream fi;
-    ofstream fo;
-    Anime_list al;
-    char name[100], to_be_updated[100], to_be_deleted[100], updated_anime_name[100], updated_manga_status, mng, del, anime_completed;
+    ifstream fi; // input stream 
+    ofstream fo; // output stream 
+    Anime_list al; // object for the structure
+
+    char name[100], to_be_updated[100], to_be_deleted[100], updated_anime_name[100], updated_manga_status, mng, del, anime_completed, anime_season, updated_anime_season;
     int ep, ch, choice, updated_episode, flag, mng_chap, updated_manga_chap;
+    
     do{
         cout<<"\n1. Add \n2. Modify \n3. Display \n4. Delete \n5. Exit \nEnter your choice: ";
         cin>>ch;
         cin.clear();
         cin.sync();
         switch(ch){
-            case 1:
+// Add - gets the required input, stores it in the structure and writes it into the binary file
+            case 1: 
                     cout<<"\n<----Add---->";
                     fo.open("anime.dat", ios::out | ios::binary | ios::app);
                     cout<<"\n Anime name: ";
@@ -35,6 +38,9 @@ int main(){
                     cin>>anime_completed;
                     
                     if(anime_completed == 'n'){
+                        cout<<" Enter the season: ";
+                        cin>>anime_season;
+                        al.season;
                         cout<<" Enter the recent episode watched: ";
                         cin>>ep;
                         al.recent_ep_watched = ep;
@@ -66,7 +72,8 @@ int main(){
                     }
                     cout<<"\n<----Exit---->";
                     break;
-        
+
+// Updating - checks if file can be opened, checks if data exists, asks on what to update, and updates
             case 2: 
                     flag = 0;
                     fi.open("anime.dat", ios::in | ios::binary);
@@ -99,6 +106,9 @@ int main(){
                                                     cin>>anime_completed;
                                                     al.completed = anime_completed;
                                                     if(al.completed == 'n'){
+                                                        cout<<" Enter the season: ";
+                                                        cin>>updated_anime_season;
+                                                        al.season = updated_anime_season;
                                                         cout<<" Enter recent episode watched: ";
                                                         cin>>updated_episode;
                                                         al.recent_ep_watched = updated_episode;
@@ -130,14 +140,16 @@ int main(){
                                 fo.close();
                             }
                             else{
+
                                 fo.open("update.dat", ios::out | ios::binary | ios::app);
                                 fo.write((char*)&al, sizeof(Anime_list));
                                 fo.close();
                             }
                         }
                         if(!flag){
-                                cout<<"RECORD NOT FOUND!";
-                                return 1;
+                                cout<<"\nRECORD NOT FOUND!\n";
+                                remove("update.dat");
+                                break;
                         }
                     fi.close();
                     remove("anime.dat");
@@ -145,6 +157,7 @@ int main(){
                     cout<<"\n<----Exit---->";
                     break;
 
+// Display - checks if file can be opened, and displays all the data
             case 3:
                     cout<<"\n<----Display---->";
                     flag = 0;
@@ -155,18 +168,21 @@ int main(){
                     }
                     while(fi.read((char*)&al, sizeof(Anime_list))){
                         cout<<"\n------------------------------------------------------------------\n\n";
-                        cout<<"\tAnime name: "<<al.anime_name<<endl;
-                        cout<<"\tAnime completed: "<<al.completed<<endl;
-                        if(al.completed == 'n')
-                            cout<<"\tRecent episode watched: "<<al.recent_ep_watched<<endl;
-                        cout<<"\tManga read: "<<al.manga<<endl;
+                        cout<<"\tAnime name ................. "<<al.anime_name<<endl;
+                        cout<<"\tAnime completed ............ "<<al.completed<<endl;
+                        if(al.completed == 'n'){
+                            cout<<"\tSeason ..................... "<<al.season<<endl;
+                            cout<<"\tRecent episode watched ..... "<<al.recent_ep_watched<<endl;
+                        }
+                        cout<<"\tManga read ................. "<<al.manga<<endl;
                         if(al.manga == 'y')
-                            cout<<"\tRecent manga chapter read: "<<al.recent_chap_read<<endl;
+                            cout<<"\tRecent manga chapter read .. "<<al.recent_chap_read<<endl;
                     }
                     fi.close();
                     cout<<"\n<----Exit---->";
                     break;
 
+// Delete - asks confirmation to delete, checks if file can be opened, checks if data exists, and deletes
             case 4: 
                     cout<<"\n<----Delete---->";
                     cout<<"\nAre you sure you want to delete an anime from the list (y/n): ";
@@ -187,16 +203,16 @@ int main(){
                                     flag = 1;
                                     cout<<"\nAnime removed from the list"<<endl;
                                 }
-                                else{
+                                else{ 
                                     fo.open("delete.dat", ios::out | ios::binary | ios::app);
                                     fo.write((char*)&al, sizeof(Anime_list));
                                     fo.close();
                                 }
                             }
                             if(!flag){
-                                cout<<"\n RECORD NOT FOUND!";
+                                cout<<"\n RECORD NOT FOUND!\n";
                                 remove("delete.dat");
-                                return 1;
+                                break;
                             }
                             fi.close();
                             remove("anime.dat");
@@ -204,9 +220,13 @@ int main(){
                     }
                     cout<<"\n<----Exit---->";                
                     break;
+            
+// Exit - ends the program execution
             case 5: 
                     cout<<".\n.\nExiting anime binary\n\n";
                     return 1; 
+
+// Any other inuput - warning for any other input
             default: 
                     cout<<"\nINVALID COMMAND\n";
                     break;
